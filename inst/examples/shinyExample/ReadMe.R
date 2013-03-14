22e6ef2261be749b46b6f2477a8b59aca19035be
Jessica Larson <larsonj5@gene.com>
2013-03-14 16:19:14 +0000

    Updated vignettes and shiny example
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ReportingTools@74333 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/examples/shinyExample/ReadMe.R
@@ -0,0 +1,10 @@
+##Run these two lines from the shinyExample directory to see 
+##ReportingTools and shiny togther.
+
+##For more details on the server.R and ui.R files see the 
+##shiny tutorial at http://rstudio.github.com/shiny/tutorial/
+
+##Look over shinyReportingTools.Rnw for more details
+
+library(shiny)
+runApp()

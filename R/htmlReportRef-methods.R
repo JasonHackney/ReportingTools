911aa32371c9e453e413bb68c67a1706a822cef7
Gabriel Becker <becker.gabriel@gene.com>
2013-03-14 23:20:01 +0000

    forgot a file
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ReportingTools@74363 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/htmlReportRef-methods.R
@@ -0,0 +1,22 @@
+setMethod(   '[[<-', c(x="HTMLReportRef"),  function(x, i, ...,value)
+    {
+      x$addElement(name = i, value = value, ...)
+      x
+    })
+
+setMethod("[[", c(x="HTMLReportRef"),  function(x, i, exact = TRUE) 
+    x$.report[[i, exact = exact]])
+ 
+setMethod("publish", 
+    signature = signature(object = "ANY", publicationType = "HTMLReportRef"), 
+    definition = function(object, publicationType, .addColumns = NULL, 
+        .toDF = NULL, ...) 
+        publicationType$addElement(value = object, ..., 
+            .addColumns = .addColumns, .toDF = .toDF ))
+
+setMethod("finish",
+    signature = signature(publicationType = "HTMLReportRef"),
+    definition = function(publicationType, ...){
+        publicationType$finish(...)
+    }
+)

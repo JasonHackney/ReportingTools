d906c78f221a84bf7f548d7723c92492cc930050
Jessica Larson <larsonj5@gene.com>
2013-03-15 16:45:47 +0000

    fixed how images are loaded
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ReportingTools@74390 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/objectToHTML-methods.R
+++ R/objectToHTML-methods.R
@@ -194,14 +194,12 @@ setMethod("objectToHTML",
                                         #pdf.url <- paste(baseUrl(publicationType), 
                                         #                 reportDirectory(publicationType), 'figures', 
                                         #                   paste(filename, 'pdf', sep='.'), sep="/")
-   # pdf.url <- paste(htmlRep$baseUrl,
-   pdf.url <- paste(htmlRep$reportDirectory, figures.dirname, 
+   pdf.url <- paste(figures.dirname, 
                      paste(filename, 'pdf', sep='.'), sep="/")
     
     png.filename <- file.path(figure.directory, 
                               paste(filename, 'png', sep='.'))
-    #png.url <- paste(htmlRep$baseUrl,
-     png.url <- paste(htmlRep$reportDirectory, figures.dirname, 
+     png.url <- paste(figures.dirname, 
                      paste(filename, 'png', sep='.'), sep="/")
             
     cairo_pdf(filename = pdf.filename, height = pdf.height, width = pdf.width)
